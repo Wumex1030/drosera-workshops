0c683e637181b9ce105ce065186c3688f6bde1ce
Wumex1030 <Wumex1030@gmail.com>
2025-08-09 12:28:12 +0100

    Update Trap.t.sol
    Signed-off-by: Wumex1030 <Wumex1030@gmail.com>

--- workshops/workshop-1/test/Trap.t.sol
+++ workshops/workshop-1/test/Trap.t.sol
@@ -1,12 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Test} from "forge-std/Test.sol";
+import "forge-std/Test.sol";
+import "../src/LargeTransferTrap.sol";
 
-contract TrapTest is Test {
-    function setUp() public {}
+contract MockERC20 {
+    mapping(address => uint256) public balances;
 
-    function test_trap() public {
-        assertTrue(true);
+    function setBalance(address account, uint256 amount) external {
+        balances[account] = amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
+
+contract LargeTransferTrapTest is Test {
+    LargeTransferTrap trap;
+    MockERC20 token;
+
+    address constant TARGET = 0x0000000000000000000000000000000000000002;
+
+    function setUp() public {
+        token = new MockERC20();
+
+        // Cheatcode: overwrite TOKEN slot in trap
+        trap = new LargeTransferTrap();
+
+        // Set initial balance
+        token.setBalance(TARGET, 10_000_000 ether);
+    }
+
+    function testDetectsLargeTransferIncrease() public {
+        bytes memory prevData = abi.encode(LargeTransferTrap.CollectOutput({balance: 10_000_000 ether}));
+        bytes memory latestData = abi.encode(LargeTransferTrap.CollectOutput({balance: 11_500_000 ether}));
+
+        bytes ;
+        data[0] = latestData;
+        data[1] = prevData;
+
+        (bool should, bytes memory msgData) = trap.shouldRespond(data);
+
+        assertTrue(should);
+        assertEq(string(msgData), "Large transfer detected");
+    }
+
+    function testNoTriggerForSmallChange() public {
+        bytes memory prevData = abi.encode(LargeTransferTrap.CollectOutput({balance: 10_000_000 ether}));
+        bytes memory latestData = abi.encode(LargeTransferTrap.CollectOutput({balance: 10_100_000 ether}));
+
+        bytes ;
+        data[0] = latestData;
+        data[1] = prevData;
+
+        (bool should, ) = trap.shouldRespond(data);
+        assertFalse(should);
     }
 }
